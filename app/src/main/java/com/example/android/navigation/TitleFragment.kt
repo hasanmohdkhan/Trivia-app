f2b0c180ba11925c936a8081a518c7922eee7c72
hasanZian <hasanzian@gmail.com>
2019-06-15 07:54:45 +0530

    added new title fragment
    Signed-off-by: hasanZian <hasanzian@gmail.com>

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -20,6 +21,13 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
                 R.layout.fragment_title, container, false)
+
+//        binding.playButton.setOnClickListener {view: View ->
+//           // view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
 
         return binding.root
     }
